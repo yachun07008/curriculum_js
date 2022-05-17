fdf44918578aaa2e90af5049db9885c7f038e55a
yachun07008 <105362037+yachun07008@users.noreply.github.com>
2022-05-17 16:38:57 +0900

    一章「HelloWorld」課題
    main.jsの作成

---
+++ main.js
@@ -0,0 +1,31 @@
+// ＝＝＝課題＝＝＝
+// 問１
+console.log("10" + "+" + "8");
+console.log(10 + 8);
+console.log("20割る3の余りは" + 20 % 3);
+// 問２
+// 問３
+
+
+console.log("");
+console.log("");
+console.log("下記学習内容");
+// ＝＝＝学習内容＝＝＝
+console.log('Hello World!!');
+console.log("Hello " + "World!!")
+console.log(37623);
+console.log(10 + 4);
+console.log(20 / 5);
+
+// 割り算の余り値を求めたい時は、「％」を使用します。
+console.log(10 % 2);
+
+/*
+上記の場合、10割る余りは「０」
+そのため出力は「０」
+下記の場合、出力は「１」になります。
+*/
+
+console.log(10 % 3);
+
+window.alert("Hello World!!")
