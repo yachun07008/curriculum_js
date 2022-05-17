487caf33e0700997be75d4156c39f55df04c35a5
yachun07008 <105362037+yachun07008@users.noreply.github.com>
2022-05-17 16:46:53 +0900

    一章「変数・定数」
    main.jsの作成

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
-// ＝＝＝課題＝＝＝
-// 問１
-console.log("10" + "+" + "8");
-console.log(10 + 8);
-console.log("20割る3の余りは" + 20 % 3);
-// 問２
-// 問３
+// ===課題===
+let price = 100;
+console.log(price * 2);
 
+price = 500;
+console.log(price * 2);
+
+// 定数const→再定義できるletに変更
 
 console.log("");
 console.log("");
-console.log("下記学習内容");
-// ＝＝＝学習内容＝＝＝
-console.log('Hello World!!');
-console.log("Hello " + "World!!")
-console.log(37623);
-console.log(10 + 4);
-console.log(20 / 5);
+console.log("学習内容");
+// ===学習内容===
+let name = "hoge";
+console.log(name);
+console.log("name");
 
-// 割り算の余り値を求めたい時は、「％」を使用します。
-console.log(10 % 2);
+name = ("fuga");
+console.log(name);
 
-/*
-上記の場合、10割る余りは「０」
-そのため出力は「０」
-下記の場合、出力は「１」になります。
-*/
+let name2 = "池田";
+console.log(name2 + "くん");
+let number = 10;
+console.log(number * 3);
 
-console.log(10 % 3);
-
-window.alert("Hello World!!")
+console.log(`${name2}くんは${number}月生まれです`);
